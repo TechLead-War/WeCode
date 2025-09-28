51c1c784892ae39fb5652181dfa3575ff9056c83
TechLead-War <ayushkapri.richard@gmail.com>
2025-09-28 14:26:04 +0530

    fixup! Adding timelines

--- app/wecode/page.js
+++ app/wecode/page.js
@@ -1,15 +1,18 @@
+import Navbar from "@/app/components/Navbar";
 import WeCodeTimeline from "@/app/components/DecodeTimeline";
 
 export const metadata = {
   title: "wecode Club Timeline | wecode",
-  description: "Timeline of events for the wecode Club."
+  description: "Timeline of events for the wecode Club.",
 };
-
 
 export default function DecodePage() {
   return (
-    <div className="min-h-screen py-8">
-      <WeCodeTimeline />
+    <div className="min-h-screen bg-black text-white mt-16 dark">
+      <Navbar />
+      <main className="max-w-7xl mx-auto px-4 py-10">
+        <WeCodeTimeline />
+      </main>
     </div>
   );
 }
